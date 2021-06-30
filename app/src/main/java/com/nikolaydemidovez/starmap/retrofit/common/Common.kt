11cd1a3ee8de88f6b705647ce8a47ddcd2a39cb0
Demidovez <nikolaydemidovez@gmail.com>
2021-06-30 16:04:17 +0300

    replace server ip

--- app/src/main/java/com/nikolaydemidovez/starmap/retrofit/common/Common.kt
+++ app/src/main/java/com/nikolaydemidovez/starmap/retrofit/common/Common.kt
@@ -4,7 +4,7 @@ import com.nikolaydemidovez.starmap.retrofit.RetrofitClient
 import com.nikolaydemidovez.starmap.retrofit.interfaces.RetrofitServices
 
 object Common {
-    private const val BASE_URL = "http://80.78.247.152:3000/"
+    private const val BASE_URL = "http://62.75.195.219:3000/"
 
     val retrofitService: RetrofitServices
         get() = RetrofitClient.getClient(BASE_URL).create(RetrofitServices::class.java)
